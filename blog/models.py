d27a487075db14c3e82f5f1e2ed57c7368183c09
mr-dargi <dargimohammadreza@gmail.com>
2025-04-13 15:12:51 +0330

    This is blog website project for first steps I add a models in blog app models

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"comment by {self.author}"
