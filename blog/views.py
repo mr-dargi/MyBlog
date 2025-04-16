d439f7d28a904fe190ff2619f900ceba39ce5f80
mr-dargi <dargimohammadreza@gmail.com>
2025-04-16 13:29:21 +0330

    Create post_list and post_detail view and its url in blog and config

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post, Comment
+from django.http import HttpResponse
+from .forms import CommentForm
 
-# Create your views here.
+
+"""
+Create view for show all posts in database
+"""
+def post_list(request):
+    try:
+        posts = Post.objects.all().order_by("-created_at")
+        return render(request, "blog/post_list.html", {"posts": posts})
+    except:
+        return render(request, "blog/post_list.html", {"posts": "there is no post"})
+
+
+"""
+View to dispaly a single post with its comments and comment form
+"""
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.mehtod == "POST":
+        form = CommentForm(request.post)
+        if form.is_valid():
+            commnet = form.save(commit=False)
+            commnet.post = post
+            commnet.save()
+            return redirect("blog:post_detail", pk=post.pk)
+    else:
+        form = CommentForm()
+    
+    return render(request, "blog/post_detail.html", {
+        "post": post,
+        "form": form
+    })
