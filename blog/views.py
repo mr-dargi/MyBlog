cb9ca2ebe5d53248099cca1a565bc8c7f5bcb479
mr-dargi <dargimohammadreza@gmail.com>
2025-04-25 15:10:20 +0330

    Add paginator

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Post, Comment
 from django.http import HttpResponse
 from .forms import CommentForm
@@ -9,7 +10,12 @@ Create view for show all posts in database
 """
 def post_list(request):
     posts = Post.objects.all().order_by("-create_at")
-    return render(request, "blog/post_list.html", {"posts": posts})
+    paginator = Paginator(posts, 5)
+    # Get current page number from GET parameter (?page=2)
+    page_number = request.GET.get("page")
+    # Get the page object
+    page_obj = paginator.get_page(page_number)
+    return render(request, "blog/post_list.html", {"page_obj": page_obj})
 
 
 """
